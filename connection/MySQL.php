a1f1b8aaeedab0e2df9571608d6ccc30e4f48e8a
tommylans <tommy.lanser@gmail.com>
2018-04-21 20:04:50 +0200

    #3 Comments verwerkt van Niels

--- connection/MySQL.php
+++ connection/MySQL.php
@@ -1,26 +1,28 @@
 <?php
-class MySQL {
+
+class MySQL
+{
 
     private static $connection;
+
     /**
      * @return null|PDO
      */
     public static function getConnection()
     {
-        if (isset(self::$connection)) {
+        if (isset(self::$connection))
             return self::$connection;
-        } else {
-            $user = "root";
-            $password = "";
-            $database = "garage";
 
-            try {
-                self::$connection =  new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-                return self::$connection;
-            } catch (PDOException $e) {
-                echo $e->getMessage();
-                return null;
-            }
+        $user = "root";
+        $password = "";
+        $database = "garage";
+
+        try {
+            self::$connection = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+            return self::$connection;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return null;
         }
     }
 }
