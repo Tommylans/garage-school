a54405deb4a3939f535eeea5dc5e4e8510014075
tommylans <tommy.lanser@gmail.com>
2018-04-21 20:53:13 +0200

    #5 Config host foutje opgelost

--- connection/MySQL.php
+++ connection/MySQL.php
@@ -16,7 +16,7 @@ class MySQL
         $config = require __DIR__ . "/../config.php";
 
         try {
-            self::$connection = new PDO("mysql:host={$config['naam']};dbname={$config['database']}", $config['user'], $config['password']);
+            self::$connection = new PDO("mysql:host={$config['host']};dbname={$config['database']}", $config['user'], $config['password']);
             return self::$connection;
         } catch (PDOException $e) {
             echo $e->getMessage();
